346f123b19f0e605f9cd5aec08d21f0e7293993a
Joél Collins <joelsewhere@gmail.com>
2025-05-08 11:12:41 -0500

    dropping build files

--- web_qmd/build/lib/web_qmd/components/__init__.py
+++
@@ -1,2 +0,0 @@
-from .basic_component import BasicComponent
-from .combined_component import CombinedComponent
--- web_qmd/build/lib/web_qmd/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .roots import find_root, get_route_base
-from .modules import load_module
